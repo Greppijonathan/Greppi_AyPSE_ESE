4f80134b9139c1dd51007b0f942275e385486d2f
Jonathan Greppi <jonathangreppi@gmail.com>
2026-07-23 12:21:40 -0300

    Primer implementacion: Sensor de temperatura + mostrar en pantalla

--- firmware/apps/main.c
+++ firmware/apps/main.c
@@ -15,7 +15,7 @@ void app_main(void)
             // Actualizamos la pantalla OLED
             board_mostrar_temperatura_oled(temp);
             
-            vTaskDelay(pdMS_TO_TICKS(1000));
+            vTaskDelay(pdMS_TO_TICKS(5000));
         }
     } else {
         printf("Error! El hardware no pudo inicializarse!\n");
